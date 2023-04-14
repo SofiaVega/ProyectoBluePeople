7ce6eb62ba90f85faa6400599037515d9df4c412
Sofia Vega <sofia.vega18@gmail.com>
2023-04-14 16:29:18 -0600

    Configurar tema

--- Notify/app/(tabs)/config_tema.tsx
+++ Notify/app/(tabs)/config_tema.tsx
@@ -2,30 +2,66 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import ComponenteTemaConfig from '../../components/ComponenteTemaConfig';
+
+type Tema = {
+  titulo: string;
+  descripcion: string;
+};
 
 export default function ConfigTemaScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Configuraciones Tema</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/config_tema.tsx" />
+    <View >
+      <ComponenteTemaConfig titulo = "Titulo" descripcion = "Descripcion"></ComponenteTemaConfig>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  temaContainer: {
+    backgroundColor:'#E8F1F2',
+    borderRadius: 10,
+    margin: 10,
+    padding: 10
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+
+    color: 'black'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  textoTema: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+    color: 'black'
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
   },
 });
+
