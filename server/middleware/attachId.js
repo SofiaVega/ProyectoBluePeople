49e86520561583bbd3bb5de9d355158abf2600ac
Luis Yerik Arambula <A00825080@itesm.mx>
2023-04-15 16:53:05 -0600

    add new routes

--- server/middleware/attachId.js
+++ server/middleware/attachId.js
@@ -1,10 +1,26 @@
-function attachId(req, res, next) {
+const pool = require("../db.js");
+
+const attachId = async (req, res, next) => {
   const user_id = req.headers["x-user-id"];
   if (!user_id) {
     return res.status(400).json({ error: "User not logged." });
   }
+  // Check if ID exists in the database
+  try {
+    const result = await pool.query("SELECT * FROM usuario WHERE id = $1", [
+      user_id,
+    ]);
+    if (result.rows.length === 0) {
+      // If ID does not exist in the database, send an error response
+      return res.status(401).json({ error: "User not found" });
+    }
+  } catch (err) {
+    // Handle database error
+    console.error("Error checking email in database:", err.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
   req.user_id = user_id;
   next();
-}
+};
 
 module.exports = attachId;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const { Pool } = require("pg");
-
 const attachId = require("./middleware/attachId");
+const pool = require("./db.js");
 
 const app = express();
 app.use(express.json());
-
-// Create a new database connection pool
-const pool = new Pool({
-  user: "postgres",
-  database: "notifydb",
-});
 
 // Test the database connection
 pool.query("SELECT NOW()", (err, res) => {
@@ -26,9 +19,20 @@ app.get("/hello", (req, res) => {
   res.send("hello");
 });
 
-//get all users route, should be Admin only
+//get all users route, admin only
 app.get("/api/users", attachId, async (req, res) => {
   try {
+    const user_id = req.user_id;
+    //Check if user is admin
+    const user_status = await pool.query(
+      "SELECT is_admin FROM usuario where id = $1",
+      [user_id]
+    );
+    const user_admin = user_status.rows[0].is_admin;
+    //If flag is false, user is not admin
+    if (!user_admin) {
+      res.status(401).send("Unauthorized");
+    }
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM usuario");
     const users = result.rows;
@@ -89,24 +93,102 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-//Get user topics
-app.get("/api/topic", attachId, async (req, res) => {
+//Create new topic route, admin only
+app.post("/api/topic", attachId, async (req, res) => {
   try {
     const user_id = req.user_id;
+    //Check if user is admin
     const user_status = await pool.query(
       "SELECT is_admin FROM usuario where id = $1",
       [user_id]
     );
     const user_admin = user_status.rows[0].is_admin;
-    if (user_admin) {
-      const title = req.title;
-      const description = req.description;
+    //If flag is false, user is not admin
+    if (!user_admin) {
+      res.status(401).send("Unauthorized");
     }
-    res.status(200).json({ user_admin });
+    //Get the rest of the data from the form
+    const { title, description, accesoMensajesPrev, cod } = req.body;
+    const tema_id = await pool.query(
+      "INSERT INTO temas (titulo, admin_id, descripcion, accessoMensajesPrev, cod) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+      [title, user_id, description, accesoMensajesPrev, cod]
+    );
+    res.status(201).json(tema_id.rows);
   } catch (err) {
-    console.log("uwu");
     console.log(err.message);
-    res.send(err);
+    res.sendStatus(500);
+  }
+});
+
+//Get all topics - admin only
+app.get("/api/topic", attachId, async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    //Check if user is admin
+    const user_status = await pool.query(
+      "SELECT is_admin FROM usuario where id = $1",
+      [user_id]
+    );
+    const user_admin = user_status.rows[0].is_admin;
+    //If flag is false, user is not admin
+    if (!user_admin) {
+      res.status(401).send("Unauthorized");
+    }
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM temas");
+    const topics = result.rows;
+    client.release();
+    res.json(topics);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: "Error getting topics" });
+  }
+});
+
+//Route for subscribing to a topic
+app.post("/api/subscribe/:id", attachId, async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    const topic_id = req.params.id;
+    //Check if topic exists
+    const result = await pool.query("SELECT * FROM temas WHERE id = $1", [
+      topic_id,
+    ]);
+    if (result.rows.length === 0) {
+      // If ID does not exist in the database, send an error response
+      return res.status(401).json({ error: "Topic not found" });
+    }
+    //If topic exists, create new entry in tema_sus
+    await pool.query(
+      "INSERT INTO tema_sus (temas_id, suscriptor_id) VALUES ($1, $2)",
+      [topic_id, user_id]
+    );
+    res.status(201).send("Success!");
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: "Error subscribing" });
+  }
+});
+
+//Route for checking topics for a user
+app.get("/api/subscriptions", attachId, async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    //Get user topics, query for title and description
+    const result = await pool.query(
+      "SELECT temas.titulo, temas.descripcion FROM tema_sus JOIN temas ON tema_sus.suscriptor_id = temas.id WHERE suscriptor_id = $1",
+      [user_id]
+    );
+    if (result.rows.length === 0) {
+      // If no topics found
+      return res
+        .status(401)
+        .json({ error: "User is not subscribed to any topics" });
+    }
+    res.status(201).send(result.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: "Error subscribing" });
   }
 });
 
