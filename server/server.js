e80a979308d97d4a8d8ef52811f20fe57cddaead
Luis Yerik Arambula <A00825080@itesm.mx>
2023-04-15 14:58:31 -0600

    change routes for new DB

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(express.json());
 
 // Create a new database connection pool
-const pool = new Pool({});
+const pool = new Pool({
+  user: "postgres",
+  database: "notifydb",
+});
 
 // Test the database connection
 pool.query("SELECT NOW()", (err, res) => {
@@ -27,12 +30,12 @@ app.get("/hello", (req, res) => {
 app.get("/api/users", attachId, async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query("SELECT * FROM users");
+    const result = await client.query("SELECT * FROM usuario");
     const users = result.rows;
     client.release();
     res.json(users);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     res.status(500).json({ message: "Error getting users" });
   }
 });
@@ -42,20 +45,23 @@ app.post("/api/register", async (req, res) => {
   try {
     const { name, email } = req.body;
     const existingUser = await pool.query(
-      "SELECT * FROM users WHERE email = $1",
+      "SELECT * FROM usuario WHERE email = $1",
       [email]
     );
+    //Check if user email repeats
     const users = existingUser.rows;
     if (users.length) {
       return res.status(400).json({ message: "User already exists" });
     }
-    const id = await client.query(
-      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
-      [name, email]
+    //Admins need to be created directly, not from API
+    const id = await pool.query(
+      "INSERT INTO usuario (nombre, email, is_admin) VALUES ($1, $2, $3) RETURNING id",
+      [name, email, false]
     );
+    //Return user ID for further authentication
     res.status(201).json(id.rows);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     res.sendStatus(500);
   }
 });
@@ -64,18 +70,21 @@ app.post("/api/register", async (req, res) => {
 app.post("/api/login", async (req, res) => {
   try {
     const { email } = req.body;
+    //Return user ID using email
     const existingUser = await pool.query(
-      "SELECT id FROM users WHERE email = $1",
+      "SELECT id FROM usuario WHERE email = $1",
       [email]
     );
     const users = existingUser.rows;
     if (users.length) {
+      //On success return ID
       res.status(201).json(users[0]);
     } else {
+      //User doesn't exist
       return res.status(400).json({ message: "User not found" });
     }
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     res.sendStatus(500);
   }
 });
@@ -85,7 +94,7 @@ app.get("/api/topic", attachId, async (req, res) => {
   try {
     const user_id = req.user_id;
     const user_status = await pool.query(
-      "SELECT is_admin FROM users where id = $1",
+      "SELECT is_admin FROM usuario where id = $1",
       [user_id]
     );
     const user_admin = user_status.rows[0].is_admin;
@@ -95,6 +104,8 @@ app.get("/api/topic", attachId, async (req, res) => {
     }
     res.status(200).json({ user_admin });
   } catch (err) {
+    console.log("uwu");
+    console.log(err.message);
     res.send(err);
   }
 });
