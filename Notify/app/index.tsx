42253dbafc4864efefb36647b5caa7e13222a88b
Luis Yerik Arambula <A00825080@itesm.mx>
2023-04-16 14:04:37 -0600

    merge nueva suscripcion

--- Notify/app/index.tsx
+++ Notify/app/index.tsx
@@ -1,77 +1,84 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from "react-native";
 
-import ComponenteTemaFila from '../components/ComponenteTemaFila';
-import ComponenteHeader from '../components/ComponenteHeader';
-import { Text, View } from '../components/Themed';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Topic } from '../interface';
-import React, {useEffect, useState} from 'react';
+import ComponenteTemaFila from "../components/ComponenteTemaFila";
+import ComponenteHeader from "../components/ComponenteHeader";
+import { Text, View } from "../components/Themed";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Topic } from "../interface";
+import React, { useEffect, useState } from "react";
 
 const parseUserData = (user: Topic) => {
-  const { titulo, descripcion, id } = user
-  return { 
-    titulo: titulo, 
+  const { titulo, descripcion, id } = user;
+  return {
+    titulo: titulo,
     descripcion: descripcion,
-    id: id
-  }
-}
+    id: id,
+  };
+};
 
 export default function PaginaPrincipalScreen() {
   const [state, setState] = useState<Topic[]>([]);
 
   useEffect(() => {
-    const api = async() => {
-      try{
+    const api = async () => {
+      try {
         const data = await fetch("http://localhost:3000/api/subscriptions", {
           method: "GET",
           headers: {
-            'x-user-id': '5',
-            'Content-Type': 'application/json',
-          }
-
+            "x-user-id": "3",
+            "Content-Type": "application/json",
+          },
         });
-        if(!data.ok) {
-          console.error(`API responded with status ${data.status}: ${data.statusText}`)
+        if (!data.ok) {
+          console.error(
+            `API responded with status ${data.status}: ${data.statusText}`
+          );
         }
 
         const jsonData = await data.json();
-        const topics = jsonData.map(parseUserData)
-        return setState([...state, ...topics])
+        const topics = jsonData.map(parseUserData);
+        return setState([...state, ...topics]);
         // return setState(jsonData.results);
-      }catch(e) {
-          console.error(e)
+      } catch (e) {
+        console.error(e);
       }
     };
     api();
   }, []);
 
-  console.log("ESTADPPP", state)
-  
+  console.log("ESTADPPP", state);
+
   return (
     <SafeAreaView>
       {/* Header bar */}
       <ComponenteHeader></ComponenteHeader>
-      <View style= {[{marginLeft: 30, marginTop: 30, marginBottom: 20}, {backgroundColor: '#E8F1F2'}]}>
-
-      <Link href="/modal_nuevo_tema" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="plus"
-                    size={25}
-                    color="black"
-                    style={[{ marginRight: 15, opacity: pressed ? 0.5 : 1 }, styles.white_background]}
-                  />
-                )}
-              </Pressable>
-            </Link>
-
+      <View
+        style={[
+          { marginLeft: 30, marginTop: 30, marginBottom: 20 },
+          { backgroundColor: "#E8F1F2" },
+        ]}
+      >
+        <Link href="/modal_nuevo_tema" asChild>
+          <Pressable>
+            {({ pressed }) => (
+              <FontAwesome
+                name="plus"
+                size={25}
+                color="black"
+                style={[
+                  { marginRight: 15, opacity: pressed ? 0.5 : 1 },
+                  styles.white_background,
+                ]}
+              />
+            )}
+          </Pressable>
+        </Link>
       </View>
       <View style={styles.temaContainer}>
         <Text style={styles.title}>Temas</Text>
-        <ComponenteTemaFila comps = {state} ></ComponenteTemaFila>
+        <ComponenteTemaFila comps={state}></ComponenteTemaFila>
         {/* <ComponenteTemaFila titulo = "Tema2" ultimaNotif = 'Ultima notif' sinLeer={4} ></ComponenteTemaFila> */}
       </View>
     </SafeAreaView>
@@ -80,28 +87,26 @@ export default function PaginaPrincipalScreen() {
 
 const styles = StyleSheet.create({
   temaContainer: {
-    backgroundColor:'white',
+    backgroundColor: "white",
     borderRadius: 10,
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 50,
-    fontWeight: 'bold',
-    color: 'black',
-    paddingLeft: 30
-
+    fontWeight: "bold",
+    color: "black",
+    paddingLeft: 30,
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
-  white_background:{
-    backgroundColor:'white',
-
-  }
+  white_background: {
+    backgroundColor: "white",
+  },
 });
