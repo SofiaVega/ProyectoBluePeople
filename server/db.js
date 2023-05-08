4512b0819f99db9507c15c986e826db5fbd76648
Sofia Vega <sofia.vega18@gmail.com>
2023-05-07 23:25:48 -0600

    debugging

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 // Create a new database connection pool
 const pool = new Pool({
   user: "postgres",
-  database: "notifydb",
-  password: "grow",
+  database: "NotifyDB",
+  password: "",
 });
 
 // Export the pool object so that it can be imported and used in other files
