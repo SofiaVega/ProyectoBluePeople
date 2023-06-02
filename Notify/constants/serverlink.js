728151848ea6d1cd1a286f7db8a02e7c2f2e9f4c
Joel Nuñez <jng@192.168.1.16>
2023-06-02 12:28:59 -0500

    fix

--- Notify/constants/serverlink.js
+++ Notify/constants/serverlink.js
@@ -1,2 +1,2 @@
-const ngrok_url = `https://30ea-2806-230-4026-bd3f-18ac-dfd6-a4a0-c278.ngrok-free.app`;
+const ngrok_url = 'https://ff55-2806-230-4026-bd3f-3836-73be-99d6-8d58.ngrok-free.app';
 export default ngrok_url;
